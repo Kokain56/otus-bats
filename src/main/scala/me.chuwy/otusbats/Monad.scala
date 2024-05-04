46e7c3c5b184f9463466c63b2096d68e8748e84f
Dmitriy <dmitriy.iv00@gmail.com>
2024-05-04 19:38:43 +0300

    Revert "Homework 8"
    This reverts commit d3421d3f54480796809713d60b88a8299e396de9.

--- src/main/scala/me.chuwy/otusbats/Monad.scala
+++ src/main/scala/me.chuwy/otusbats/Monad.scala
@@ -1,7 +1,5 @@
 package me.chuwy.otusbats
 
-import scala.annotation.tailrec
-
 trait Monad[F[_]] extends Functor[F] { self =>
   def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
 
@@ -11,56 +9,5 @@ trait Monad[F[_]] extends Functor[F] { self =>
 }
 
 object Monad {
-  implicit val opt: Monad[Option] = new Monad[Option] {
-    override def flatMap[A, B](fa: Option[A])(f: A => Option[B]): Option[B] = fa match {
-      case Some(v) => f(v)
-      case None => None
-    }
-
-    override def point[A](a: A): Option[A] = Option(a)
-
-    override def flatten[A](fa: Option[Option[A]]): Option[A] = flatMap(fa)(x => x)
-
-    override def map[A, B](fa: Option[A])(f: A => B): Option[B] = fa.map(f)
-  }
-
-  implicit val list: Monad[List] = new Monad[List] {
-
-    override def flatMap[A, B](fa: List[A])(f: A => List[B]): List[B] = {
-
-      @tailrec
-      def flatMap(list: List[A], acc: List[B]): List[B] = list match {
-        case Nil => acc
-        case head :: tail => flatMap(tail, (f(head) ::: acc))
-      }
-      flatMap(fa, List.empty[B])
-    }
-
-    override def point[A](a: A): List[A] = List(a)
-
-    override def flatten[A](fa: List[List[A]]): List[A] = fa.flatMap(x => x)
-
-    override def map[A, B](fa: List[A])(f: A => B): List[B] = fa.map(f)
-  }
-
-  implicit val seq: Monad[Seq] = new Monad[Seq] {
-    override def flatMap[A, B](fa: Seq[A])(f: A => Seq[B]): Seq[B] = fa.flatMap(f)
-
-    override def point[A](a: A): Seq[A] = Seq(a)
-
-    override def flatten[A](fa: Seq[Seq[A]]): Seq[A] = flatMap(fa)(x => x)
-
-    override def map[A, B](fa: Seq[A])(f: A => B): Seq[B] = fa.map(f)
-  }
-
-  def apply[F[_]](implicit monad: Monad[F]): Monad[F] = monad
-
-  implicit class flattenImpl[F[_] : Monad, A](mon: F[F[A]]) {
-    def flatten: F[A] = Monad[F].flatten[A](mon)
-  }
-
-  implicit class flatMapImpl[F[_] : Monad, A, B](mon: F[A]){
-    def flatMap(f: A => F[B]) : F[B] = Monad[F].flatMap[A, B](mon)(f)
-  }
 
 }
--- src/main/scala/me.chuwy/otusbats/Show.scala
+++ src/main/scala/me.chuwy/otusbats/Show.scala
@@ -1,7 +1,5 @@
 package me.chuwy.otusbats
 
-import scala.annotation.unused
-
 
 trait Show[A] {
   def show(a: A): String
@@ -10,35 +8,21 @@ trait Show[A] {
 object Show {
 
   // 1.1 Instances (`Int`, `String`, `Boolean`)
-  implicit val intToString: Show[Int] = new Show[Int] {
-    override def show(a: Int): String = a.toString
-  }
-
-  implicit val stringToString: Show[String] = new Show[String] {
-    override def show(a: String): String = a
-  }
-
-  implicit val booleanToString: Show[Boolean] = new Show[Boolean] {
-    override def show(a: Boolean): String = a.toString
-  }
 
 
   // 1.2 Instances with conditional implicit
 
-  implicit def listShow[A](implicit @unused ev: Show[A]): Show[List[A]] =
-    new Show[List[A]] {
-      override def show(a: List[A]): String = a.toString()
-    }
+  implicit def listShow[A](implicit ev: Show[A]): Show[List[A]] =
+    ???
 
 
   // 2. Summoner (apply)
-  def apply[T](implicit show: Show[T]): Show[T] = show
 
   // 3. Syntax extensions
 
   implicit class ShowOps[A](a: A) {
     def show(implicit ev: Show[A]): String =
-      ev.show(a)
+      ???
 
     def mkString_[B](begin: String, end: String, separator: String)(implicit S: Show[B], ev: A <:< List[B]): String = {
       // with `<:<` evidence `isInstanceOf` is safe!
@@ -48,7 +32,6 @@ object Show {
 
   }
 
-
   /** Transform list of `A` into `String` with custom separator, beginning and ending.
    *  For example: "[a, b, c]" from `List("a", "b", "c")`
    *
@@ -57,19 +40,15 @@ object Show {
    *  @param end. ']' in above example
    */
   def mkString_[A: Show](list: List[A], begin: String, end: String, separator: String): String =
-    list.mkString(begin, separator, end)
+    ???
 
 
   // 4. Helper constructors
 
   /** Just use JVM `toString` implementation, available on every object */
-  def fromJvm[A]: Show[A] = new Show[A] {
-    override def show(a: A): String = a.toString
-  }
+  def fromJvm[A]: Show[A] = ???
   
   /** Provide a custom function to avoid `new Show { ... }` machinery */
-  def fromFunction[A](f: A => String): Show[A] =   new Show[A] {
-    override def show(a: A): String = f(a)
-  }
+  def fromFunction[A](f: A => String): Show[A] = ???
 
 }
